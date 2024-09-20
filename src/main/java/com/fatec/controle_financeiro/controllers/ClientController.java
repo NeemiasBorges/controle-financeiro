c9843913fbe6b5a7ae3834b0c6f5af21848b9d78
Neemias Borges <neemias.matos@fatec.sp.gov.br>
2024-09-19 21:49:03 -0300

    feat: adição do fornecedor e testes no postman

--- src/main/java/com/fatec/controle_financeiro/controllers/ClientController.java
+++ src/main/java/com/fatec/controle_financeiro/controllers/ClientController.java
@@ -12,46 +12,48 @@ public class ClientController {
     private List<Cliente> clientes = new ArrayList<>();
     private int nextId = 1;
 
-    // Create (POST)
     @PostMapping
-    public Cliente criarCliente(@RequestBody Cliente cliente) {
+    public Cliente criarCliente(@RequestBody Cliente cliente)
+    {
         cliente.setId(nextId++);
+        
         clientes.add(cliente);
         return cliente;
     }
 
-    // Read (GET) - Listar todos os clientes
     @GetMapping
-    public List<Cliente> listarClientes() {
+    public List<Cliente> listarClientes() 
+    {
         return clientes;
     }
 
-    // Read (GET) - Obter um cliente específico por id
     @GetMapping("/{id}")
-    public Cliente obterCliente(@PathVariable int id) {
+    public Cliente obterCliente(@PathVariable int id)
+    {
         return clientes.stream()
                 .filter(c -> c.getId() == id)
                 .findFirst()
                 .orElse(null);
     }
 
-    // Update (PUT)
     @PutMapping("/{id}")
-    public Cliente atualizarCliente(@PathVariable int id, @RequestBody Cliente clienteAtualizado) {
+    public Cliente atualizarCliente(@PathVariable int id,
+    @RequestBody Cliente cliUpdate) 
+    {
         for (int i = 0; i < clientes.size(); i++) {
             Cliente c = clientes.get(i);
             if (c.getId() == id) {
-                clienteAtualizado.setId(id);
-                clientes.set(i, clienteAtualizado);
-                return clienteAtualizado;
+                cliUpdate.setId(id);
+                clientes.set(i, cliUpdate);
+                return cliUpdate;
             }
         }
         return null;
     }
 
-    // Delete (DELETE)
     @DeleteMapping("/{id}")
-    public boolean removerCliente(@PathVariable int id) {
+    public boolean removerCliente(@PathVariable int id)
+    {
         return clientes.removeIf(c -> c.getId() == id);
     }
 }
---
+++ src/main/java/com/fatec/controle_financeiro/entities/Fornecedor.java
@@ -0,0 +1,5 @@
+package com.fatec.controle_financeiro.entities;
+
+public class Fornecedor {
+    
+}
